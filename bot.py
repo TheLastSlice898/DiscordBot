06e802aa3073fdaad314826d0a9f9a4081ce13c1
TheLastSlice898 <56330886+TheLastSlice898@users.noreply.github.com>
2025-04-24 18:18:33 +1000

    Started Main File with DB access

--- bot.py
+++ bot.py
@@ -1,28 +1,81 @@
-import discord
 from dotenv import *
-import os
-#Checklist 
-# Make a
-# #Vert cocool 
-
 load_dotenv()
+import asyncio
+import os
+import time
+import discord
+from discord.abc import Messageable
+from discord.ext import commands
+from supabase import create_client
+#Checklist 
+#Make a
+##Vert cocool 
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'longged on as {self.user}')
-
-    async def on_message(self, message):
-        print(f'message is from {message.author}: {message.content}')
-
-
-intents = discord.Intents.default()
+BOT_ID = os.getenv("DISCORD_APPLICATION_ID")
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase = create_client(url, key)
+sen_ID = 1184811030807846972
+#intialisation of the bot perms
+intents = intents=discord.Intents.default()
 intents.message_content = True
+#Create Bot Class
+bot = commands.Bot(command_prefix='+',intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'longged on as {bot.user}')
+
+@bot.command()
+async def rank(self):
+    #wait 2 sec
+    async with self.typing():
+        await asyncio.sleep(2)
+    await self.send("I cant find shit pookie")
+
+@bot.event
+async def on_message(message: discord.Message):
+   #Check so that it doesnt 
+   if message.author == bot.user:
+       return
+   
+   if bot.user in message.mentions:
+      await message.add_reaction(":ferret_huh:1319980301874757643")
+   #if message.author.id == 251263598364459008:
+   #   if "fuck off" in message.content.lower():
+   #       async with message.channel.typing():
+   #            await asyncio.sleep(1)
+   #       await message.channel.send("ow okay il fuck off")
+   #       await shutdown_bot(message.channel)
+   #   else:   
+   #    await message.reply('Hey Slice')
+   else:
+        print(f"{message.author} sent {message.content}")
+        await bot.process_commands(message)
 
 
 
+async def shutdown_bot(channel: discord.abc.Messageable):
+    await channel.send('https://tenor.com/view/spy-visit-your-mother-gif-1664119401268449295')
+    await bot.close()
 
 
-client = Client(intents=intents)
-client.run(BOT_TOKEN)
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await shutdown_bot(ctx)
+
+discord_id = input("what ID you do want the xp for")
+response = supabase.table("Discord-Bot-XP").select("xp").eq("discord_id",discord_id).single().execute()
+
+print(response.data["xp"])
+
+#old client bot which is cringe ;3
+#class Client(discord.Client):
+#    async def on_ready(self):
+#        print(f'longged on as {self.user}')
+#
+#    async def on_message(self, message):
+#        print(f'message is from {message.author}: {message.content}')
+bot.run(BOT_TOKEN)
