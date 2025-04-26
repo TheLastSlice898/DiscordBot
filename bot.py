f8e39bcecd82bef97b744239c87d9ab1d1b2f20a
TheLastSlice898 <56330886+TheLastSlice898@users.noreply.github.com>
2025-04-26 17:22:51 +1000

    Added the +setup Functionality

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 from dotenv import *
 load_dotenv()
+import json
 import asyncio
+import requests
 import os
 import time
 import discord
 from discord.abc import Messageable
 from discord.ext import commands
+from discord.ext.commands import Context
 from supabase import create_client
 #Checklist 
 #Make a
@@ -57,11 +60,136 @@ async def on_ready():
 
 
 @bot.command()
-async def rank(self):
+async def rank(ctx):
     #wait 2 sec
-    async with self.typing():
+    async with ctx.typing():
         await asyncio.sleep(2)
-    await self.send("I cant find shit pookie")
+    await ctx.send("I cant find shit pookie")
+
+@bot.command()
+async def setup(ctx: Context):
+    async with ctx.typing():
+        await asyncio.sleep(1)
+    msg = await ctx.send('Do you want the bot to ping on level up')
+    await msg.add_reaction('🟩')
+    await msg.add_reaction('🟥')
+    try:
+        user = await bot.wait_for('reaction_add', timeout=10.0)
+    except asyncio.TimeoutError:
+        await ctx.send('Sorry I didnt get a respose :(')
+    else:
+        print(f'{user[0]}')
+        if user[0].emoji == '🟩':
+            try:
+                supabase.table('Discord_Bot_Settings').update({'ping_user_on_levelup': True}).eq('guild_id',ctx.guild.id).execute()
+            except Exception as e:
+                print(e)
+            async with ctx.typing():
+                await asyncio.sleep(0.5)
+            await ctx.send('Updated to Ping Users')
+        if user[0].emoji == '🟥':
+            try:
+                supabase.table('Discord_Bot_Settings').update({'ping_user_on_levelup': False}).eq('guild_id',ctx.guild.id).execute()
+            except Exception as e:
+                print(e)
+            async with ctx.typing():
+                    await asyncio.sleep(0.5)
+            await ctx.send('Updated to Not send to users')
+    msg = await ctx.send('Do you want the bot to msg in the channel you have leveled in or msg in a dedicated channel,🟩 for message in the channel,🟥 for a dedicated channel  ')
+    await msg.add_reaction('🟩')
+    await msg.add_reaction('🟥')
+    try:
+        user = await bot.wait_for('reaction_add', timeout=10.0)
+    except asyncio.TimeoutError:
+        await ctx.send('Sorry I didnt get a respose :(')
+    else:
+        print(f'{user[0]}')
+        if user[0].emoji == '🟩':
+            try:
+                supabase.table('Discord_Bot_Settings').update({'msg_in_channel': True}).eq('guild_id',ctx.guild.id).execute()
+            except Exception as e:
+                print(e)
+            async with ctx.typing():
+                await asyncio.sleep(0.5)
+            await ctx.send('The bot will message in the channel')
+        if user[0].emoji == '🟥':
+            try:
+                supabase.table('Discord_Bot_Settings').update({'msg_in_channel': True}).eq('guild_id',ctx.guild.id).execute()
+            except Exception as e:
+                print(e)
+            async with ctx.typing():
+                await asyncio.sleep(0.5)
+            await ctx.send('The bot will message in a dedicated channel')
+            async with ctx.typing():
+                await asyncio.sleep(0.5)
+            await ctx.send('Can you give me the channel ID for that channel')
+            def check(message: discord.Message):
+                return message.author == ctx.author and message.channel == ctx.channel
+            try:
+                user_msg = await bot.wait_for('message',timeout=60.0,check=check)
+                channel_id = int(user_msg.content)
+
+                #verify that mf exists
+                if ctx.guild.get_channel(channel_id):
+                    supabase.table('Discord_Bot_Settings').update({'xp_channel':channel_id}).eq('guild_id',ctx.guild.id).execute()
+                    await ctx.send(f'Cool! I got <#{channel_id}>')
+                else:
+                    await ctx.send('I could not find a channel with that ID ( ͡° ͜ʖ ͡°)')
+            except asyncio.TimeoutError as e:
+                await ctx.send('I timed out :(')
+            except ValueError:
+                await ctx.send('That was not a valid number ????')
+            else:
+                pass
+    msg = await ctx.send('Do you want to any channels to have XP gain ignored')
+    await msg.add_reaction('🟩')
+    await msg.add_reaction('🟥')
+    try:
+        user = await bot.wait_for('reaction_add', timeout=10.0)
+    except asyncio.TimeoutError:
+        await ctx.send('Sorry I didnt get a respose :(')
+    else:
+        if user[0].emoji == '🟩':
+            await ctx.send('Can you give me a list of channel ID'+'s seperate by a , for example '+'1165648362868060220,1294533854639427594,1130184098711879782')
+            def check(message: discord.Message):
+                return message.author == ctx.author and message.channel == ctx.channel
+            try:
+                user_msg = await bot.wait_for('message',timeout=60.0,check=check)
+                channel_ids = user_msg.content.split(',')
+                listofchannels = []
+                listofchecks = [bool]
+                for id in channel_ids:
+                    if ctx.guild.get_channel(int(id)):
+                        listofchecks.append(True)
+                        listofchannels.append(f'<#{id}>')
+                    else:
+                        listofchecks.append(False)
+                def check_all_ids(data):
+                    return all(data)
+                if check_all_ids(listofchecks):
+                    await ctx.send(f'Updated the list to {listofchannels}')
+                    supabase.table('Discord_Bot_Settings').update({'ignored_channels': channel_ids}).execute()
+                else:
+                    await ctx.send('That wasnt a valid list')
+            except asyncio.TimeoutError as e :
+                await ctx.send("I timed out :8")
+            except ValueError:
+                await ctx.send('That wasnt a Valid List')
+                   
+
+        if user[0].emoji == '🟥':
+            await ctx.send('Cool everything should be setup')
+            supabase.table('Discord_Bot_Settings').update({'is_setup':True}).eq('guild_id',ctx.guild.id).execute()
+
+
+
+                
+
+
+
+
+        
+
 
 @bot.event
 async def on_message(message: discord.Message):
@@ -85,8 +213,8 @@ async def on_message(message: discord.Message):
 
 
 
-async def shutdown_bot(channel: discord.abc.Messageable):
-    await channel.send('https://tenor.com/view/spy-visit-your-mother-gif-1664119401268449295')
+async def shutdown_bot(ctx: discord.abc.Messageable):
+    await ctx.send('https://tenor.com/view/spy-visit-your-mother-gif-1664119401268449295')
     await bot.close()
 
 
@@ -95,10 +223,11 @@ async def shutdown_bot(channel: discord.abc.Messageable):
 async def shutdown(ctx):
     await shutdown_bot(ctx)
 
-discord_id = input("what ID you do want the xp for")
-response = supabase.table("Discord-Bot-XP").select("xp").eq("discord_id",discord_id).single().execute()
+#discord_id = input("what ID you do want the xp for")
 
-print(response.data["xp"])
+
+
+#print(response.data["xp"])  
 
 #old client bot which is cringe ;3
 #class Client(discord.Client):
